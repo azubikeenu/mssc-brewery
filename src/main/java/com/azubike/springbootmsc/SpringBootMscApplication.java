d3dcabb96a7ab30d57eaa9f993c8083af6b1c85d
Enu Azubike <enuazubike88@gmail.com>
2023-02-15 15:12:15 +0100

    initial commit

---
+++ src/main/java/com/azubike/springbootmsc/SpringBootMscApplication.java
@@ -0,0 +1,13 @@
+package com.azubike.springbootmsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azubike/springbootmsc/SpringBootMscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azubike.springbootmsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
