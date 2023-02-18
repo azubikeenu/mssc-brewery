f63f418279309d57df0b6de59a3a01f0741d1eda
Enu Azubike <enuazubike88@gmail.com>
2023-02-18 23:29:22 +0100

    added support for RestDocs and fixed BeerControllerTest to support restdocs

--- src/test/java/com/azubike/mssc_brewery/web/controller/BeerControllerTest.java
+++ src/test/java/com/azubike/mssc_brewery/web/controller/BeerControllerTest.java
@@ -5,20 +5,29 @@ import com.azubike.mssc_brewery.web.services.BeerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BeerController.class)
+// --- This was commented to integrate restDocs---------//
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+@WebMvcTest(BeerController.class)
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 class BeerControllerTest {
   @MockBean BeerService beerService;
@@ -28,18 +37,20 @@ class BeerControllerTest {
 
   @BeforeEach
   void setup() {
-    beerDto = BeerDto.builder().beerStyle("PALE_ALE")
-            .beerName("notNull")
-            .upc(10000L)
-            .build();
+    beerDto = BeerDto.builder().beerStyle("PALE_ALE").beerName("notNull").upc(10000L).build();
   }
 
   @Test
   void getBeerById() throws Exception {
 
     mockMvc
-        .perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+        .perform(get("/api/v1/beer/{beerId}", UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "v1/beer",
+                pathParameters(
+                    parameterWithName("beerId").description("UUID of desired beer to get"))));
   }
 
   @Test
--- target/generated-sources/annotations/com/azubike/mssc_brewery/web/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/com/azubike/mssc_brewery/web/mappers/CustomerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-02-18T20:19:42+0100",
+    date = "2023-02-18T23:25:05+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
